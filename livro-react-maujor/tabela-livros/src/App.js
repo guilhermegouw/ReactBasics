cc500f1caa463e0f61bfc9717cae8048fd6cfa55
guilhermegouw <guilherme.gouw@gmail.com>
2022-05-17 12:30:28 -0300

    05/17/2022

--- livro-react-maujor/tabela-livros/src/App.js
+++ livro-react-maujor/tabela-livros/src/App.js
@@ -24,26 +24,56 @@ class App extends Component {
     const livros = this.state.livros.filter(l => l.id !== id);
     this.setState({livros})
   };
-  handleOrdenarCrescente = (titulo) => {
+  handleOrdenarIsbnCrescente = (titulo) => {
+    const livros = this.state.livros.sort((a, b) =>
+    a.id < b.id ? -1: 0
+    );
+    this.setState({ livros });
+  };
+  handleOrdenarIsbnDecrescente = titulo => {
+    const livros = this.state.livros.sort((a, b) =>
+    a.id < b.id ? -1: 0
+    );
+    livros.reverse();
+    this.setState({ livros });
+  };
+  handleOrdenarTituloCrescente = (titulo) => {
     const livros = this.state.livros.sort((a, b) =>
     a.titulo < b.titulo ? -1: 0
     );
     this.setState({ livros });
   };
-  handleOrdenarDecrescente = titulo => {
+  handleOrdenarTituloDecrescente = titulo => {
     const livros = this.state.livros.sort((a, b) =>
     a.titulo < b.titulo ? -1: 0
     );
     livros.reverse();
     this.setState({ livros });
   };
+  handleOrdenarAutorCrescente = (autor) => {
+    const livros = this.state.livros.sort((a, b) =>
+    a.autor < b.autor ? -1: 0
+    );
+    this.setState({ livros });
+  }
+  handleOrdenarAutorDecrescente = (autor) => {
+    const livros = this.state.livros.sort((a, b) =>
+    a.autor < b.autor ? -1: 0
+    );
+    livros.reverse();
+    this.setState({ livros });
+  }
 
   render () {
     return (
       <table className="tabela">
         <TabelaHead 
-        ordenarCrescente={ this.handleOrdenarCrescente }
-        ordenarDecrescente={ this.handleOrdenarDecrescente }
+        ordenarTituloCrescente={ this.handleOrdenarTituloCrescente }
+        ordenarTituloDecrescente={ this.handleOrdenarTituloDecrescente }
+        ordenarAutorCrescente={ this.handleOrdenarAutorCrescente }
+        ordenarAutorDecrescente={ this.handleOrdenarAutorDecrescente }
+        ordenarIsbnCrescente={ this.handleOrdenarIsbnCrescente }
+        ordenarIsbnDecrescente={ this.handleOrdenarIsbnDecrescente }
         />
         <TabelaFoot qdeLivros={this.state.livros.length}/>
         <TabelaBody 
--- livro-react-maujor/tabela-livros/src/components/TabelaHead.js
+++ livro-react-maujor/tabela-livros/src/components/TabelaHead.js
@@ -5,14 +5,24 @@ const TabelaHead = (props) => (
             <th colSpan="4">Tabela de Livros</th>
         </tr>
         <tr>
-            <th>ISBN</th>
-            <th>Título
+            <th>ISBN
                 <div className="container-setinhas">
-                    <div onClick={ () => props.ordenarCrescente()}>&#11014;</div>
-                    <div onClick={ () => props.ordenarDecrescente()}>&#11015;</div>
+                    <div onClick={ () => props.ordenarIsbnCrescente()}>&#11014;</div>
+                    <div onClick={ () => props.ordenarIsbnDecrescente()}>&#11015;</div>
                 </div>
             </th>
-            <th>Author</th>
+            <th>Título
+                <div className="container-setinhas">
+                    <div onClick={ () => props.ordenarTituloCrescente()}>&#11014;</div>
+                    <div onClick={ () => props.ordenarTituloDecrescente()}>&#11015;</div>
+                </div>
+            </th>
+            <th>Author
+                <div className="container-setinhas">
+                    <div onClick={ () => props.ordenarAutorCrescente()}>&#11014;</div>
+                    <div onClick={ () => props.ordenarAutorDecrescente()}>&#11015;</div>
+                </div>
+            </th>
             <th></th>
         </tr>
     </thead>
